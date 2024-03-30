93adfc0aee5dec4e586e2fa660d4624daabee127
Yosi Ben Shushan <Yosi.Ben.shushan1@s.afeka.ac.il>
2024-03-30 20:19:14 +0300

    initial commit

---
+++ app/src/main/java/com/example/gotcha/Activities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.gotcha.Activities;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.gotcha.Fragments.AddItemFragment;
+import com.example.gotcha.Fragments.CategoriesFragment;
+import com.example.gotcha.Fragments.HomeFragment;
+import com.example.gotcha.R;
+import com.example.gotcha.Fragments.SettingsFragment;
+import com.example.gotcha.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment()); // when the user open the app he will see the home fragment
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            }else if(item.getItemId() == R.id.categories){
+                replaceFragment(new CategoriesFragment());
+            }else if(item.getItemId() == R.id.add_item){
+                replaceFragment(new AddItemFragment());
+            }else{
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout , fragment);
+        fragmentTransaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/gotcha/Logics/DataManager.java
@@ -0,0 +1,4 @@
+package com.example.gotcha.Logics;
+
+public class DataManager {
+}
